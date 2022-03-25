90af9be6056f57a823dd709e19d1f21fcf70ebd5
CaptainCredible <cpt.credible@gmail.com>
2022-03-25 20:06:51 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,34 +12,32 @@ let myAcc = 0
 let wasAB = false
 let average = 0
 
-
 let highThresh = 1500
 let lowThresh = 400
-
+let wasStop = false
+let wasReset = false
 let highDurationThresh = 10
 let lowDurationThresh = 250
 let DEBUG = false
 let allowedToCount = false
 let myAvgArray: number[] = []
 let jumps = 0 // counter for jumps
-
-
 allowedToCount = true
-music.playTone(440, 200)
-if (DEBUG) {
-    allowedToCount = true
-}
+//
+
 basic.showNumber(myNumber, 100)
 basic.showIcon(IconNames.Skull)
 basic.clearScreen()
+led.plot(2,4)
+radio.setGroup(1)
+
+music.playTone(440, 200)
+
 
 let movingAverageCount = 5
 for (let i = 0; i <= movingAverageCount - 1; i++) {
     myAvgArray[i] = 0
 }
-basic.showIcon(IconNames.StickFigure)
-radio.setGroup(1)
-
 
 function averaged(newVal: number) {
     myAvgArray.pop()
@@ -48,6 +46,7 @@ function averaged(newVal: number) {
     average = sum / myAvgArray.length
     return average
 }
+
 function countJump() {
     if (allowedToCount) {
         jumps += 1
@@ -56,9 +55,10 @@ function countJump() {
         music.playTone(440, 50)
     }
     if (DEBUG) {
-        radio.sendValue("jumps", jumps)
+        //radio.sendValue("jumps", jumps)
     }
 }
+
 input.onButtonPressed(Button.A, function () {
     if (DEBUG) {
         jumps += -1
@@ -73,6 +73,7 @@ input.onButtonPressed(Button.B, function () {
 })
 
 radio.onReceivedValue(function (name, value) {
+    led.toggle(2,0)
     if (name == "count") {
         if (myNumber != 0) {
             control.waitMicros(10000 * myNumber)
@@ -98,18 +99,26 @@ radio.onReceivedValue(function (name, value) {
         basic.showString("LD ", 80)
         basic.showNumber(value, 80)
     } else if (name == "reset") {
-        // set counts to 0
+        if(!wasReset){
+            music.ringTone(1000)
+        }
+        
+        wasReset = true
         jumps = 0
-        music.playTone(1000, 50)
         allowedToCount = true
-        basic.clearScreen()
-        led.plot(2, 1)
+        led.toggleAll()
     } else if (name == "stop") {
+        if(!wasStop){
+            music.ringTone(2000)
+        }
+        wasStop = true
         // STOP THE COUNT!!!
         allowedToCount = false
         basic.showIcon(IconNames.No, 0)
     }
 })
+
+
 
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     if (DEBUG) {
@@ -117,7 +126,20 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     }
 })
 
+
+
 loops.everyInterval(1000, function () {
+    if(wasReset){
+        music.stopAllSounds()
+        basic.clearScreen()
+        wasReset = false
+    }
+
+    if(wasStop){
+        music.stopAllSounds()
+        wasStop = false
+    }
+    
     if (input.buttonIsPressed(Button.AB) && input.logoIsPressed()) {
         if (wasAB) {
             DEBUG = !(DEBUG)
@@ -148,38 +170,29 @@ loops.everyInterval(1000, function () {
 })
 
 basic.forever(function() {
+    led.toggle(2,4)
     myAcc = Math.abs(input.acceleration(Dimension.Y))
     myAvgAcc = averaged(myAcc)
-    // serial.writeValue("raw", myAcc)
-    // serial.writeValue("avg", myAvgAcc)
     if (DEBUG) {
-        radio.sendValue("avg", myAvgAcc)
+        //radio.sendValue("avg", myAvgAcc)
     }
-    // MEASURE HIGH ACCELERATION
     if (myAvgAcc > highThresh && !(wasHigh)) {
-        // went above high acceleration thresh
         wasHigh = true
-        // radio.sendValue("high", 1)
-        if (DEBUG) {
-
-        }
         highStart = input.runningTime()
     } else if (myAvgAcc < highThresh && wasHigh) {
         wasHigh = false
-        // radio.sendValue("high", 0)
-        if (DEBUG) {
-
-        }
         highDuration = input.runningTime() - highStart
         if (highDuration > highDurationThresh) {
             if (allowedToCount) {
                 if (!(jumped)) {
                     jumped = true
-                    led.toggle(2, 1)
-                    led.toggle(2, 3)
+                    led.unplot(1, 1)
+                    led.unplot(2, 1)
+                    led.unplot(3, 1)
+                    led.plot(2, 3)
                     music.playTone(660, 10)
                 } else {
-                    music.playTone(550, 10)
+                    music.playTone(330, 10)
                 }
             }
         }
@@ -201,15 +214,13 @@ basic.forever(function() {
                 music.playTone(880, 20)
                 if (jumped) {
                     jumped = false
-                    led.toggle(2, 1)
-                    led.toggle(2, 3)
+                    led.plot(1, 1)
+                    led.plot(2, 1)
+                    led.plot(3, 1)
+                    led.unplot(2, 3)
                     countJump()
                 }
             }
         }
     }
-
-})
-
-loops.everyInterval(10, function () {
 })
